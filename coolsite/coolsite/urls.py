1e404a53325a8aeda29bdbf23f600fc3d8df0d25
Rodion <79993057+Rodion0io@users.noreply.github.com>
2022-07-09 13:49:06 +0700

    'Test version'

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from firstapp.views import lox
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('loshara/',lox), #http://127.0.0.1:8000/loshara/
+
 ]
